426a30d5f7cca5a4dbe55b7a0a9b2b9d26026ac6
SidheLore <103224878+SidheLore@users.noreply.github.com>
2022-09-27 05:42:41 -0400

    index.js start

---
+++ index.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const dbConnect = mysql.createConnection({
+    host:
+    user:
+    password:
+    database:
+})
+
+switch(){
+    case
+    {
+        break;
+    }
+    case
+    {
+        break;
+    }
+}
